387abe718701447801692de0e5273d36bb6fb8ec
Mukhongovivian-prog <vivianmukhongo72@gmail.com>
2025-06-04 14:23:30 +0300

    modified files

--- Thrive/Thrive/settings.py
+++ Thrive/Thrive/settings.py
@@ -49,10 +49,13 @@ INSTALLED_APPS = [
     'learning',
     'benefits',
     'community',
+    "corsheaders",
 ]
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +64,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://serenity-one-tau.vercel.app",
+    ]
 
 ROOT_URLCONF = 'Thrive.urls'
 
