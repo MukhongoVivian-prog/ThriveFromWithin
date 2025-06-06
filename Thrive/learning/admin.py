16352f77fe86a09254118d02b60f43dc41edab2d
Mukhongovivian-prog <vivianmukhongo72@gmail.com>
2025-06-06 17:07:35 +0300

    installed jwt

--- Thrive/learning/admin.py
+++ Thrive/learning/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Guide,Webinar,Toolkit
 
 # Register your models here.
+admin.site.register(Guide)
+admin.site.register(Webinar)
+admin.site.register(Toolkit)
