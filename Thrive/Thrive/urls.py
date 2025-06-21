436578a9f13403247edaad0250d68be5c030f06c
EmmanuelAttah1 <attahzuzu@gmail.com>
2025-06-21 18:55:00 +0100

    setup django static files server

--- Thrive/Thrive/urls.py
+++ Thrive/Thrive/urls.py
@@ -4,6 +4,9 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Thrive From Within API",
@@ -24,5 +27,5 @@ urlpatterns = [
    #  path('api/SafeRoom/', include('SafeRoom.urls')),
    #  path('api/wellness/', include('wellness.urls')),
    # path('api/benefits/', include('benefits.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
